cb53413d961c0254edd921db41954d49279c653e
siennang <siennangu65@gmail.com>
2024-08-08 18:24:59 -0400

    updated package.json

--- src/components/common/card.jsx
+++ src/components/common/card.jsx
@@ -1,3 +1,26 @@
+
+import React from "react";
+
+import "./styles/card.css";
+
+const Card = (props) => {
+	const {title, body } = props;
+	return (
+		<div className="card">
+			<div className="card-container">
+				<div className="card-header">
+					<div className="card-title">{title}</div>
+				</div>
+				<div className="card-body">
+					<div className="card-text">{body}</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
+
 import React from "react";
 
 import "./styles/card.css";
--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -1,3 +1,4 @@
+
 // Importing React library to use React functionalities
 import React from "react";
 
@@ -32,3 +33,39 @@ const Footer = () => {
 
 // Exporting the Footer component as the default export from this module
 export default Footer;
+
+// Importing React library to use React functionalities
+import React from "react";
+
+// Importing Link component from react-router-dom for navigation links
+import { Link } from "react-router-dom";
+
+// Importing CSS styles specific to the footer component
+import "./styles/footer.css";
+
+// Defining the Footer functional component
+const Footer = () => {
+	return (
+		// React Fragment to wrap multiple elements without adding an extra node to the DOM
+		<React.Fragment>
+			{/* Main container for the footer */}
+			<div className="footer">
+				{/* Container for the footer navigation links */}
+				<div className="footer-links">
+				</div>
+
+				{/* Container for the footer credits */}
+				<div className="footer-credits">
+					{/* Text displaying the footer credits */}
+					<div className="footer-credits-text">
+						© 2024 Sienna.dev. All Rights Reserved.
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+// Exporting the Footer component as the default export from this module
+export default Footer;
+
